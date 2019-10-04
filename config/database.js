7173aea0e907e0a3676d397b7244590d557b98e3
jeanjoe <manzede@gmail.com>
2019-10-04 12:28:13 +0300

    Add useUnifiedTopology to database config

--- config/database.js
+++ config/database.js
@@ -11,22 +11,22 @@ if (process.env.NODE_ENV == 'test') {
 }
 
 const connection = () => {
-  mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true })
+  mongoose.connect(dbURL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
-  mongoose.connection.on('connected', function () {
-    console.log(connected('Mongoose default connection is open to ', dbURL))
+  mongoose.connection.on('connected', () => {
+    console.log(connected('Mongoose default connection is open :)'))
   })
 
-  mongoose.connection.on('error', function (err) {
+  mongoose.connection.on('error', err => {
     console.log(error('Mongoose default connection has occured ' + err + ' error'))
   })
 
-  mongoose.connection.on('disconnected', function () {
+  mongoose.connection.on('disconnected', () => {
     console.log(disconnected('Mongoose default connection is disconnected'))
   })
 
-  process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
+  process.on('SIGINT', () => {
+    mongoose.connection.close(() => {
       console.log(termination('Mongoose default connection is disconnected due to application termination'))
       process.exit(0)
     })
